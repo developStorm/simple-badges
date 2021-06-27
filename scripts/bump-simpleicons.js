956fb2c8edfd7b2f934b96d529993958e7af0d6d
devStorm <59678453+developStorm@users.noreply.github.com>
2021-06-27 16:51:47 -0700

    Track icon version used by Shields.io

--- scripts/bump-simpleicons.js
+++ scripts/bump-simpleicons.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 /**
  * @fileoverview
- * Updates the simple-icons dependency to the latest version.  Upon success, the
- * new simple-icons dependency version is outputted.
+ * Updates the simple-icons dependency to the same version used in Shields.io backend.
+ * Upon success, the new simple-icons dependency version is outputted.
  */
 
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
 const PACKAGE_JSON_FILE = path.resolve(__dirname, '..', 'package.json');
 
@@ -17,21 +18,34 @@ function getManifest() {
   return packageFile;
 }
 
-function main() {
+function getShieldManifest() {
+  return httpRequest(
+    'https://raw.githubusercontent.com/badges/shields/master/package.json',
+  );
+}
+
+async function main() {
   try {
     const manifestBefore = getManifest();
     const versionBefore = manifestBefore.dependencies['simple-icons'];
+    const shieldSIVersion = (await getShieldManifest()).dependencies[
+      'simple-icons'
+    ];
+
+    if (versionBefore === shieldSIVersion) {
+      console.error(
+        'Simple Icons version installed is consistent with Shields.io backend, no action needed.',
+      );
+      process.exit(0);
+    }
 
     execSync('npm uninstall simple-icons', { stdio: 'ignore' });
-    execSync('npm install --save-exact simple-icons', { stdio: 'ignore' });
+    execSync(`npm install --save-exact simple-icons@${shieldSIVersion}`, {
+      stdio: 'ignore',
+    });
 
     const manifestAfter = getManifest();
     const versionAfter = manifestAfter.dependencies['simple-icons'];
-
-    if (versionBefore === versionAfter) {
-      console.error('Simple icons does not need to be updated');
-      process.exit(0);
-    }
 
     console.log(versionAfter);
   } catch (error) {
@@ -41,3 +55,34 @@ function main() {
 }
 
 main();
+
+// https://stackoverflow.com/a/38543075
+function httpRequest(params, postData) {
+  return new Promise(function (resolve, reject) {
+    const req = https.request(params, function (res) {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error('statusCode=' + res.statusCode));
+      }
+      // cumulate data
+      let body = [];
+      res.on('data', function (chunk) {
+        body.push(chunk);
+      });
+      res.on('end', function () {
+        try {
+          body = JSON.parse(Buffer.concat(body).toString());
+        } catch (e) {
+          reject(e);
+        }
+        resolve(body);
+      });
+    });
+    req.on('error', function (err) {
+      reject(err);
+    });
+    if (postData) {
+      req.write(postData);
+    }
+    req.end();
+  });
+}
