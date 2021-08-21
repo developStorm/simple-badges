595c9baac37aaef47111e255cab95bccb09b4bcd
devStorm <59678453+developStorm@users.noreply.github.com>
2021-08-21 12:03:02 -0400

    Update search input
    Sync https://github.com/simple-icons/simple-icons-website/commit/7822108aaa09783894a7cb1594a050fd10ec3672

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -62,6 +62,10 @@ export default function initSearch(history, document, ordering, domUtils) {
     }),
   );
 
+  $searchInput.addEventListener('change', () => {
+    $searchInput.blur();
+  });
+
   $searchClear.addEventListener('click', (event) => {
     event.preventDefault();
     $searchInput.value = '';
